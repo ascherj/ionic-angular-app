290d3e794cb5c4ac9aba0bfc72b3d20559a42467
jake-ascher <jascher@diversifiedus.com>
2019-10-17 12:56:12 -0400

    Set up Angular routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
+  { 
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule'
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+    ]
+  }
 ];
 
 @NgModule({
