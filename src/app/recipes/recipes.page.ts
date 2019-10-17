21ecddc88a7811cac3daba6346b340321912d241
jake-ascher <jascher@diversifiedus.com>
2019-10-17 12:37:51 -0400

    Use Angular features on Ionic components

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1280px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Spaghettata.JPG/1280px-Spaghettata.JPG',
+      ingredients: ['Spaghetti', 'Tomatoes', 'Cheese']
+    }
+  ];
 
   constructor() { }
 
