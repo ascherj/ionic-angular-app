4ab6360198fb64076182f9c5597f7bdffb9c9eff
Jake Ascher <jascher@diversifiedus.com>
2019-10-31 14:34:22 -0400

    Prepare data and services for project

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://image.shutterstock.com/image-photo/high-angle-view-manhattan-new-600w-222155194.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris!',
+      'https://image.shutterstock.com/image-photo/beautiful-view-traditional-french-country-260nw-1036146931.jpg',
+      189.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://image.shutterstock.com/image-photo/tree-house-fog-260nw-1211127625.jpg',
+      99.99
+    ),
+  ];
+
+  get places() {
+    return [...this.places];
+  }
+
+  constructor() { }
+}
